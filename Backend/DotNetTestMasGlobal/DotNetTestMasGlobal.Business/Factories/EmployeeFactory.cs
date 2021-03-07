94c14d3f52e0a534c2d47f817bcafcd15a81470c
Julian Slonim <slonim.julian@gmail.com>
2021-03-07 13:13:47 -0300

    Comment

--- Backend/DotNetTestMasGlobal/DotNetTestMasGlobal.Business/Factories/EmployeeFactory.cs
+++ Backend/DotNetTestMasGlobal/DotNetTestMasGlobal.Business/Factories/EmployeeFactory.cs
@@ -11,6 +11,8 @@ namespace DotNetTestMasGlobal.Business.Factories
     public static class EmployeeFactory
     {
         public static EmployeeDTO CreateEmployee(EmployeeEnums.ContractTypeName contractTypeName, Employee employee) {
+            //Del enunciado entendi que se requeria un tipo de DTO distinto para cada tipo de worker, aca estan usados con el patron factory
+            //aunque sean iguales y no necesiten distincion del DTO original, usandolos para mapear de manera correspondiente la data y el AnualSalary
             if (contractTypeName == EmployeeEnums.ContractTypeName.HourlySalaryEmployee)
             {
                 return new HourlySalaryEmployee(employee);
